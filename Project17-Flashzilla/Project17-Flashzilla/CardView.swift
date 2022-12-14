c99d6817c669946c9c885547eb411ca141921017
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2022-12-14 13:34:57 +0200

    Completed Day 90
    - Added Project 17 - Flashzilla

--- Project17-Flashzilla/Project17-Flashzilla/CardView.swift
+++ Project17-Flashzilla/Project17-Flashzilla/CardView.swift
@@ -11,23 +11,46 @@ struct CardView: View {
     let card: Card
     var removal: (() -> Void)? = nil
     
+    @State private var feedback = UINotificationFeedbackGenerator()
+    
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
+    
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.yellow)
+                .fill(
+                    differentiateWithoutColor
+                    ? .yellow
+                    : .yellow
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             VStack {
-                Text(card.promt)
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.white.opacity(0.7))
+                if voiceOverEnabled {
+                    Text(isShowingAnswer ? card.answer : card.promt)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                } else {
+                    Text(card.promt)
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.white.opacity(0.7))
+                    }
                 }
             }
             .padding()
@@ -37,23 +60,30 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width)
         .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibilityAddTraits(.isButton)
         .gesture (
             DragGesture()
                 .onChanged { gesture in
                     offset = gesture.translation
+                    feedback.prepare()
                 }
                 .onEnded { _ in
                     if offset.width > 100 {
+                        if offset.width < 0 {
+                            feedback.notificationOccurred(.error)
+                        }
                         removal?()
+//                    } else if offset.width < -100 {
+//                       removal?()
                     } else {
                         offset = .zero
                     }
                 }
         )
-        
         .onTapGesture {
             isShowingAnswer.toggle()
         }
+        .animation(.spring(), value: offset)
     }
 }
 
